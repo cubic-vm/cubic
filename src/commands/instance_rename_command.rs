4a645a92f8cfb46aabc033490859e88d438dcfe8
Roger Knecht <rknecht@pm.me>
2025-08-04 18:26:42 +0000

    refactor: clean up instance rename command code
    Signed-off-by: Roger Knecht <rknecht@pm.me>

--- src/commands/instance_rename_command.rs
+++ src/commands/instance_rename_command.rs
@@ -1,19 +1,17 @@
 use crate::error::Error;
 use crate::instance::{InstanceDao, InstanceStore};
+use clap::Parser;
 
+/// Rename a virtual machine instance
+#[derive(Parser)]
 pub struct InstanceRenameCommand {
+    /// Name of the virtual machine instance to rename
     old_name: String,
+    /// New name of the virutal machine instance
     new_name: String,
 }
 
 impl InstanceRenameCommand {
-    pub fn new(old_name: &str, new_name: &str) -> Self {
-        Self {
-            old_name: old_name.to_string(),
-            new_name: new_name.to_string(),
-        }
-    }
-
     pub fn run(&self, instance_dao: &InstanceDao) -> Result<(), Error> {
         instance_dao.rename(&mut instance_dao.load(&self.old_name)?, &self.new_name)
     }
