70a678fb17579ee2036b764ecd2c675279698328
Roger Knecht <rknecht@pm.me>
2025-08-04 20:55:21 +0000

    refactor: clean up instance start command code
    Signed-off-by: Roger Knecht <rknecht@pm.me>

--- src/commands/restart.rs
+++ src/commands/restart.rs
@@ -5,8 +5,15 @@ use crate::instance::InstanceDao;
 pub fn restart(
     instance_dao: &InstanceDao,
     verbosity: Verbosity,
-    instances: &Vec<String>,
+    instances: &[String],
 ) -> Result<(), Error> {
     commands::stop(instance_dao, false, verbosity, true, instances)?;
-    commands::start(instance_dao, &None, verbosity, true, instances)
+    commands::InstanceStartCommand {
+        qemu_args: None,
+        verbose: verbosity.is_verbose(),
+        quiet: verbosity.is_quiet(),
+        wait: true,
+        instances: instances.to_vec(),
+    }
+    .run(instance_dao)
 }
