bf7c86de2e7f67393077e6c2af8f1710e95d7511
Roger Knecht <rknecht@pm.me>
2025-08-25 14:25:28 +0000

    feat: added show image command
    Signed-off-by: Roger Knecht <rknecht@pm.me>

---
+++ src/commands/show_image_command.rs
@@ -0,0 +1,32 @@
+use crate::commands::image::fetch_image_list;
+use crate::env::Environment;
+use crate::error::Error;
+use crate::image::ImageStore;
+use crate::view::{Console, MapView};
+use clap::Parser;
+
+/// Show image information
+#[derive(Parser)]
+pub struct ShowImageCommand {
+    /// Name of the virtual machine image
+    name: String,
+}
+
+impl ShowImageCommand {
+    pub fn run(
+        &self,
+        console: &mut dyn Console,
+        env: &Environment,
+        image_store: &dyn ImageStore,
+    ) -> Result<(), Error> {
+        fetch_image_list(env);
+        let image = image_store.get(&self.name)?;
+        let mut view = MapView::new();
+        view.add("Vendor", &image.vendor);
+        view.add("Codename", &image.codename);
+        view.add("Version", &image.version);
+        view.add("URL", &image.url);
+        view.print(console);
+        Ok(())
+    }
+}
