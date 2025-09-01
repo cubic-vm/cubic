1ed33e7b7d6fa6e8d4d9ddfd587f62249dd6348e
Roger Knecht <rknecht@pm.me>
2025-09-01 13:42:36 +0000

    feat: add progress bar to image download
    Signed-off-by: Roger Knecht <rknecht@pm.me>

--- src/view.rs
+++ src/view.rs
@@ -1,6 +1,7 @@
 pub mod console;
 pub mod console_mock;
 pub mod map_view;
+pub mod progress_bar;
 pub mod spinner_view;
 pub mod stdio;
 pub mod table_view;
@@ -8,6 +9,7 @@ pub mod transfer_view;
 
 pub use console::*;
 pub use map_view::*;
+pub use progress_bar::*;
 pub use spinner_view::*;
 pub use stdio::*;
 pub use table_view::*;
