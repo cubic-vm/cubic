921602bd1a10cfccdd9a8940f7f772550a8326a3
Roger Knecht <rknecht@pm.me>
2025-09-04 09:31:09 +0000

    fix: avoid selecting an used port for ssh
    Replaced the logic to select a random tcp port for SSH by a function that always
    selects a free port.
    
    Signed-off-by: Roger Knecht <rknecht@pm.me>

--- src/util/generate_random_ssh_port.rs
+++ src/util/generate_random_ssh_port.rs
@@ -1,13 +1,7 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-const PORT_MIN: u128 = 1024;
-const PORT_MAX: u128 = 65535;
+use std::net::TcpListener;
 
 pub fn generate_random_ssh_port() -> u16 {
-    (SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis()
-        % (PORT_MAX - PORT_MIN)
-        + PORT_MIN) as u16
+    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let addr = listener.local_addr().unwrap();
+    addr.port()
 }
